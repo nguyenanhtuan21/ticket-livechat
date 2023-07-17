05fda580042b05fa084f1451a178dbebe1ff71d3
nguyenanhtuan21 <tuananhnguyenyt@gmail.com>
2023-07-17 10:58:27 +0700

    init

---
+++ src/components/MisaTicket.vue
@@ -0,0 +1,32 @@
+<script setup>
+import * as LiveChat from "@livechat/agent-app-sdk";
+</script>
+<template>
+  <div>hello men</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      test: 123,
+    };
+  },
+  created() {
+    console.log(12312321);
+    this.embedPopup()
+  },
+  methods: {
+    embedPopup() {
+      LiveChat.createDetailsWidget().then((widget) => {
+        // build your logic around the widget
+        widget.on("customer_profile", (profile) => {
+          // read the new profile
+          console.log(profile);
+        });
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MisaTicket from '../components/MisaTicket.vue'
+</script>
+
+<template>
+  <main>
+    <MisaTicket />
+  </main>
+</template>
