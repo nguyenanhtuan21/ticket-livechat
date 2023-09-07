05b9c7b320e4429fff75598b0d22896bcb434b66
nguyenanhtuan21 <tuananhnguyenyt@gmail.com>
2023-09-07 09:42:47 +0700

    add

--- src/components/MisaTicket.vue
+++ src/components/MisaTicket.vue
@@ -4,12 +4,12 @@ import { Livechat } from "../object/Livechat";
 import axios from "axios";
 import { ref } from "vue";
 
-const iframeURL = ref("https://testticket.misa.vn/livechat");
+const iframeURL = ref("https://ticket.misa.vn/livechat");
 </script>
 <template>
   <div>
     <iframe
-      :src="iframeURL"
+      :src="frameURL"
       frameborder="0"
       style="width: 100%; height: 100vh"
     ></iframe>
@@ -19,12 +19,48 @@ const iframeURL = ref("https://testticket.misa.vn/livechat");
 export default {
   data() {
     return {
-      frameURL: "https://testticket.misa.vn/livechat",
+      frameURL: "https://ticket.misa.vn/livechat",
     };
   },
   async created() {
     await this.embedPopup();
-    // await this.getEmbedPopup();
+
+    /////
+    // console.log(`https://ticket.misa.vn/apis/open-api/livechat/request-link`);
+    // var cusData = {
+    //   customerName: "", // Tên công ty
+    //   phoneNumber: "", // Số điện thoại
+    //   email: "", // Email
+    //   fullName: "", // Tên đầy đủ
+    //   taxCode: "", // Mã số thuế
+    //   source: "", // Nguồn
+    //   version: "", // Phiên bản
+    //   BudgetCode: "", // Mã quan hệ ngân sách
+    // };
+    // console.log(`getembedPopup ${JSON.stringify(cusData)}`);
+    // await axios
+    //   .post(
+    //     `${import.meta.env.VITE_BASE_API_URL}${
+    //       import.meta.env.VITE_TICKET_URL
+    //     }`,
+    //     cusData,
+    //     {
+    //       headers: {
+    //         Accept: "/",
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     console.log(`fffsss ${JSON.stringify(cusData)}`);
+    //     console.log(typeof(res.data));
+    //     // this.frameURL = {...res.data};
+    //     console.log(this.frameURL+'?ver=23');
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    /////
   },
   methods: {
     async embedPopup() {
@@ -38,80 +74,58 @@ export default {
         version: "", // Phiên bản
         BudgetCode: "", // Mã quan hệ ngân sách
       };
-      var frameData = 'https://testticket.misa.vn/livechat';
-      await LiveChat.createDetailsWidget().then((widget) => {
-        // build your logic around the widget
-        widget.on("customer_profile", (profile) => {
-          cusData.customerName =
-            profile.customVariables["Công ty" || "Cty" || undefined] || "";
-          cusData.phoneNumber =
-            profile.customVariables["Điện thoại"] ||
-            profile.customVariables["Số điện thoại"] ||
-            profile.customVariables["SĐT"] ||
-            "";
-          cusData.email = profile.email;
-          cusData.fullName = profile.customVariables["Họ tên"] || "";
-          cusData.taxCode =
-            profile.customVariables["Mã số thuế"] ||
-            profile.customVariables["MST"] ||
-            "";
-          cusData.source = profile.customVariables["Nguồn"] || "";
-          cusData.version =
-            profile.customVariables["Phiên bản"] || "";
-          cusData.BudgetCode =
-            profile.customVariables["Mã QHNS"] || "";
-          console.log(456);
-          // console.log(cusData);
+      try {
+        await LiveChat.createDetailsWidget().then((widget) => {
+          // build your logic around the widget
+          widget.on("customer_profile", (profile) => {
+            if (profile) {
+              cusData.customerName =
+                profile.customVariables["Công ty" || "Cty" || undefined] || "";
+              cusData.phoneNumber =
+                profile.customVariables["Điện thoại"] ||
+                profile.customVariables["Số điện thoại"] ||
+                profile.customVariables["SĐT"] ||
+                "";
+              cusData.email = profile.email;
+              cusData.fullName = profile.customVariables["Họ tên"] || "";
+              cusData.taxCode =
+                profile.customVariables["Mã số thuế"] ||
+                profile.customVariables["MST"] ||
+                "";
+              cusData.source = profile.customVariables["Nguồn"] || "";
+              cusData.version = profile.customVariables["Phiên bản"] || "";
+              cusData.BudgetCode = profile.customVariables["Mã QHNS"] || "";
+              console.log(456);
+            }
+            // console.log(cusData);
 
-          axios
-            .post(
-              `${import.meta.env.VITE_BASE_API_URL}${
-                import.meta.env.VITE_TICKET_URL
-              }`,
-              cusData,
-              {
-                headers: {
-                  Accept: "/",
-                  "Content-Type": "application/json",
-                },
-              }
-            )
-            .then( (res) => {
-              console.log(`fff ${JSON.stringify(cusData)}`);
-              frameData = res.data;
-            })
-            .catch( (err) => {
-              console.log(err);
-            });
+            axios
+              .post(
+                `${import.meta.env.VITE_BASE_API_URL}${
+                  import.meta.env.VITE_TICKET_URL
+                }`,
+                cusData,
+                {
+                  headers: {
+                    Accept: "/",
+                    "Content-Type": "application/json",
+                  },
+                }
+              )
+              .then((res) => {
+                console.log(`fff ${JSON.stringify(cusData)}`);
+                this.frameURL = res.data;
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
         });
-      });
-
-      this.iframeURL = frameData;
-      console.log(this.iframeURL);
+      } catch (error) {
+        console.log(error);
+      }
     },
-    async getEmbedPopup() {
-      console.log(`gêtmbedPopup ${JSON.stringify(cusData)}`);
-      await axios
-        .post(
-          `${import.meta.env.VITE_BASE_API_URL}${
-            import.meta.env.VITE_TICKET_URL
-          }`,
-          cusData,
-          {
-            headers: {
-              Accept: "/",
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        .then(function (res) {
-          console.log(`fff ${JSON.stringify(cusData)}`);
-          this.iframeURL = res.data;
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    },
+    async getEmbedPopup() {},
   },
 };
 </script>
